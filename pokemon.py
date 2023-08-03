58fb0226b2466d5d13b71e731f8e4b43991a5c16
cassiano <cassianothiago@rocketmail.com>
2023-08-03 16:58:16 -0400

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -4,7 +4,32 @@ import sys
 
 
 class MainWindow(QMainWindow):
-    
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Pokedex")
+        self.setGeometry(100,100,300,150)
+        
+        self.label1 = QLabel("Pokemom 1", self)
+        self.label1.setGeometry(10,10,80,30)
+        
+        self.label2 = QLabel("Pokemo 2", self)
+        self.label2.setGeometry(10,50,80,30)
+        
+        self.input1 = QLineEdit(self)
+        self.input1.setGeometry(100,10,80,30)
+        
+        self.input2 = QLineEdit(self)
+        self.input2.setGeometry(100,50,80,30)
+        
+        self.result_label = QLabel(self)
+        self.result_label.setGeometry(10,90,280,30)
+        
+        
+        
+    def calcular(self):
+        pokemon1 = (self.input1.text())
+        pokemon2 = (self.input2.text())
+        self.result_label.setText(f"O resultado da soma é {soma}")
 
 
 app = QApplication(sys.argv)
