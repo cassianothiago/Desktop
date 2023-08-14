0570de96a15ebcd7fbec79b882823a2ce8d0c1dc
cassiano <cassianothiago@rocketmail.com>
2023-08-14 15:00:58 -0400

    update

---
+++ rev_aulas/dark.py
@@ -0,0 +1,30 @@
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QFormLayout, QWidget, QLabel,
+    QRadioButton, QCheckBox, QLineEdit, QSpinBox, QDoubleSpinBox,
+    QPushButton, QComboBox, QFontComboBox, QDateEdit, QDateTimeEdit,
+    QLCDNumber, QProgressBar, QDial, QSlider)
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPalette,QColor
+import sys
+
+app.setStyle("Fusão")
+
+paleta_escura = QPaleta()
+
+dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+dark_palette.setColor(QPalette.WindowText, Qt.white)
+dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+dark_palette.setColor(QPalette.Text, Qt.white)
+dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+dark_palette.setColor(QPalette.ButtonText, Qt.white)
+dark_palette.setColor(QPalette.BrightText, Qt.red)
+dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+
+app.setPalette(dark_palette)
+
+app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
