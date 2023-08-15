a67b56d2d04f4ff2dcb586746fae0c0681342668
cassiano <cassianothiago@rocketmail.com>
2023-08-15 08:39:51 -0400

    update

---
+++ contaBank.py
@@ -0,0 +1,56 @@
+from typing import Optional
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QFormLayout, QWidget, QLabel,
+    QRadioButton, QCheckBox, QLineEdit, QSpinBox, QDoubleSpinBox,
+    QPushButton, QComboBox, QFontComboBox, QDateEdit, QDateTimeEdit,
+    QLCDNumber, QProgressBar, QDial, QSlider)
+from PySide6.QtCore import (Qt,QSize)
+from PySide6.QtGui import (QPalette,QColor)
+import sys
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QLabel, QPushButton, QLabel, QLineEdit,
+    QVBoxLayout,QHBoxLayout)
+
+class Mainwindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle('Seu Banco')
+        self.setFixedSize(QSize(400,200))
+        
+        self.agencia=QLineEdit(self)
+        self.conta=QLineEdit(self)
+        
+        self.pergunta=QLabel('Deseja fazer deposito inicial?')
+        
+        
+        self.depositoS=QCheckBox('sim')
+        self.depositoN=QCheckBox('não')
+        '''self.depositoS.setTristate(True)
+        layoutBox = QVBoxLayout()
+        layoutBox.addWidget(self.depositoS)
+        layoutBox.addWidget(self.depositoN)
+        containerBox = QWidget()
+        containerBox.setLayout(self.depositoS)
+        containerBox.setLayout(self.depositoN)'''
+        
+        self.valorDep=QLineEdit(self)
+        
+        self.depositar=QPushButton('Depositar',self)
+        
+        pagina=QFormLayout(self)
+        pagina.addRow('Agência = ',self.agencia)
+        pagina.addRow('Conta = ',self.conta)
+        pagina.addRow(self.pergunta)
+        pagina.addRow(self.depositoS)
+        pagina.addRow(self.depositoN)
+        pagina.addRow(self.valorDep)
+        pagina.addRow(self.depositar)
+        widgetFORmulario = QWidget()
+        widgetFORmulario.setLayout(pagina)
+        self.setCentralWidget(widgetFORmulario)
+        
+app = QApplication(sys.argv)
+w = Mainwindow()
+w.show()
+app.exec()
