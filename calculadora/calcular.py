2bff4598800dcc76aca8dc12276650f4fcaccfbd
cassiano <cassianothiago@rocketmail.com>
2023-09-18 20:49:11 -0400

    update

--- calculadora/calcular.py
+++ calculadora/calcular.py
@@ -2,12 +2,11 @@ class Calculo:
     def calcular(self,expresao:str):
         self.expresao=expresao
         self.resultado=eval(self.expresao)
-        print(self.resultado)
+        return self.resultado
         
         
+
     
-calc=Calculo()
-calc.calcular('10/8')
-type(calc)
+
         
     
